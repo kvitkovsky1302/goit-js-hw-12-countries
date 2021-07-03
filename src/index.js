d13b360e805c3288f261e8d3c1a209a37fc95a4a
Kvitkovsky Maxim <kvitkovsky1302@gmail.com>
2021-07-03 16:40:27 +0300

    add pnotify

--- src/index.js
+++ src/index.js
@@ -2,21 +2,24 @@ import './sass/main.scss';
 import fetchCountries from './js/fetchCountries.js';
 import debounce from 'lodash.debounce';
 import createCard from './templates/country-render.hbs';
+import createCountryList from './templates/country-list.hbs';
 import "@pnotify/core/dist/PNotify.css";
 import "@pnotify/core/dist/BrightTheme.css";
 import "@pnotify/mobile/dist/PNotifyMobile.css";
 import "@pnotify/countdown/dist/PNotifyCountdown.css";
-import { alert } from '@pnotify/core';
+import notificationOptions from './js/notificationSettings.js';
+import { alert} from '@pnotify/core';
 
 
 
 const refs = {
-    input: document.querySelector('.input'),
-    countryList: document.querySelector('.country-list'),
+    input: document.querySelector('.autocomplete-input'),
+    countryList: document.querySelector('.result-list'),
+    countryCard: document.querySelector('.country-card'),
 }
 
 
-refs.input.addEventListener('input', debounce(searchCountry, 1000));
+refs.input.addEventListener('input', debounce(searchCountry, 500));
 
 function searchCountry(e) {
     const countryToFind = refs.input.value;
@@ -32,36 +35,36 @@ function searchCountry(e) {
 function createMarkup(countries) {
     clearMarkup();
     if (countries.length > 10) {
-        return alert('to many countries')
+        return alert(notificationOptions.toMachResults);
     };
-    if (countries.length >= 2 && countries.length <= 10) {
+    if (countries.length > 1 && countries.length <= 10) {
+        alert(notificationOptions.coupleResults);
+        refs.countryList.classList.remove('hidden')
         return createCountriesList(countries);
     }
     if (countries.length === 1) {
+        alert(notificationOptions.successResult);
         return createDetailCard(...countries);
     }
-    return  showNotification('country not found', 'error') 
+    return  alert(notificationOptions.noResult); 
 }
 
-function showNotification(text, type) {
-    return alert({
-         text,
-        type,
-        delay: 3000
-    })
-}
 
 function clearMarkup() {
     refs.countryList.innerHTML = '';
+    refs.countryCard.innerHTML = '';
+    refs.countryCard.classList.remove('visible');
 }
 
 function createCountriesList(countries) {
-    const markup = countries.reduce((acc, elem) => acc + `<li>${elem.name}</li>`, '');
-    refs.countryList.insertAdjacentHTML('beforeend', markup);
+    const resultArray = countries.map(country => country.name);
+    refs.countryList.insertAdjacentHTML("beforeend", createCountryList({ countryName: resultArray }));
 }
 
 function createDetailCard(country) {
+    clearMarkup()
+    refs.countryCard.classList.add('visible');
     const markupCountry = createCard(country);
-    refs.countryList.insertAdjacentHTML('beforeend', markupCountry);
+    refs.countryCard.insertAdjacentHTML('beforeend', markupCountry);
     refs.input.value = '';
 }
